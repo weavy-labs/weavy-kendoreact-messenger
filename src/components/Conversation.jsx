503ec5aa5bc33476b8f9f0d90a22051e4b0edd7d
Magnus Krona <magnus.krona@gmail.com>
2021-08-23 12:26:33 +0200

    cleanup

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -8,73 +8,16 @@ const Conversation = () => {
     let { id } = useParams();
     const queryClient = useQueryClient()
 
+    const { isLoading, isError, data, error } = useQuery(['messages', id], getMessages, { refetchOnWindowFocus: false });
+
     const user = {
         id: 3,
         avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
     };
+    
     const fileUpload = createRef();
 
     const [attachments, setAttachments] = useState([]);
-
-    const getMessages = async () => {
-
-        const response = await fetch("https://showcase.weavycloud.com/api/conversations/" + id + "/messages",
-            {
-                method: 'GET',
-                credentials: 'include'
-            });
-
-        const messages = await response.json();
-        console.log(messages)
-        return messages.data?.map((item) => {
-            return {
-                text: item.text,
-                timestamp: new Date(item.created_at),
-                author: {
-                    id: item.created_by.id,
-                    name: item.created_by.name,
-                    avatarUrl: `https://showcase.weavycloud.com/${item.created_by.thumb.replace('{options}', '32')}`
-                },
-                attachments: item.attachments
-                // attachments: item.attachments.map((a) => {
-                //     return {
-                //         content: "https://showcase.weavycloud.com/attachments/" + a + "/image.png",
-                //         contentType: "image",
-                //     }
-                // })
-            }
-        });
-    }
-
-    const addMessage = async (message) => {
-        console.log(attachments)
-        let files = attachments.map((f) => {
-
-            return f.id;
-        })
-        console.log(files)
-        let json = JSON.stringify({ text: message.text, blobs: files });
-
-        return fetch('https://showcase.weavycloud.com/a/conversations/' + id + '/messages', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: json
-        });
-    }
-
-    const uploadFiles = async (data) => {
-        return fetch('https://showcase.weavycloud.com/a/blobs/', {
-            method: 'POST',
-            credentials: 'include',
-            body: data
-        });
-    }
-
-    const { isLoading, isError, data, error } = useQuery(['messages', id], getMessages, { refetchOnWindowFocus: false });
 
     const addMessageMutation = useMutation(addMessage, {
         // When mutate is called:
@@ -99,10 +42,67 @@ const Conversation = () => {
         onSettled: () => {
             queryClient.invalidateQueries(['messages', id])
         },
-    })
+    });
 
-    const insertMessage = (message) => {
-        console.log(message)
+    const getMessages = async () => {
+
+        const response = await fetch("https://showcase.weavycloud.com/api/conversations/" + id + "/messages",
+            {
+                method: 'GET',
+                credentials: 'include'
+            });
+
+        const messages = await response.json();
+        
+        return messages.data?.map((item) => {
+            return {
+                text: item.text,
+                timestamp: new Date(item.created_at),
+                author: {
+                    id: item.created_by.id,
+                    name: item.created_by.name,
+                    avatarUrl: `https://showcase.weavycloud.com/${item.created_by.thumb.replace('{options}', '32')}`
+                },
+                attachments: item.attachments
+                // attachments: item.attachments.map((a) => {
+                //     return {
+                //         content: "https://showcase.weavycloud.com/attachments/" + a + "/image.png",
+                //         contentType: "image",
+                //     }
+                // })
+            }
+        });
+    }
+
+    const addMessage = async (message) => {
+        
+        let files = attachments.map((f) => {
+
+            return f.id;
+        })
+        
+        let json = JSON.stringify({ text: message.text, blobs: files });
+
+        return fetch('https://showcase.weavycloud.com/a/conversations/' + id + '/messages', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: json
+        });
+    }
+
+    const uploadFiles = async (data) => {
+        return fetch('https://showcase.weavycloud.com/a/blobs/', {
+            method: 'POST',
+            credentials: 'include',
+            body: data
+        });
+    }
+
+    const insertMessage = (message) => {        
         addMessageMutation.mutate(message);
     }
 
@@ -127,14 +127,7 @@ const Conversation = () => {
         // setMessages([...messages, event.message]);
     }
 
-    const CustomChatMessage = (props) => (
-        <ChatMessage {...props} dateFormat={"t"} />
-    );
-
-
     const handleInputChange = async (e) => {
-
-        //let file = e.target.files[0];
 
         var files = e.target.files;
 
@@ -142,41 +135,23 @@ const Conversation = () => {
             // add files to formdata object
             var formData = new FormData();
             for (let index = 0; index < files.length; index++) {
-                const file = files[index];
-                console.log(file)
+                const file = files[index];                
                 formData.append("file-" + index, file);
             }
 
             var response = await uploadFiles(formData);
             var json = await response.json();
 
-            setAttachments([...attachments, ...json.data]);
-
-            console.log(json);
-        }
-
-        // let reader = new FileReader();
-
-        // reader.onloadend = (event) => {
-        //   let message = {
-        //     author: user,
-        //     text: "",
-        //     attachments: [
-        //       {
-        //         content: event.target ? event.target.result : "",
-        //         contentType: "image",
-        //       },
-        //     ],
-        //   };
-
-        //insertMessage(message)
-        //setMessages([...messages, message]);
-        //};
-
-        //reader.readAsDataURL(file);
+            setAttachments([...attachments, ...json.data]);            
+        }       
     };
 
-    const UploadButton = (props) => {
+    const CustomChatMessage = (props) => (
+        <ChatMessage {...props} dateFormat={"t"} />
+    );
+
+
+    const CustomUploadButton = (props) => {
         return (
             <Fragment>
                 <input
@@ -212,7 +187,7 @@ const Conversation = () => {
                     })}
                 </div>
                 <div style={{display:'flex', flex: 1}}>
-                    {UploadButton({
+                    {CustomUploadButton({
                         icon: "k-i-image-insert",
                     })}
                     {props.messageInput}
@@ -221,25 +196,11 @@ const Conversation = () => {
 
                 </div>
             </Fragment>
-            // <Fragment>
-            //     {UploadButton({
-            //             icon: "k-i-image-insert",
-            //         })}
-            //         {props.messageInput}
-            //         {props.sendButton}  
-            // </Fragment>
+         
         );
     };
 
-    // const MessageTemplate = (props) => {
-    //     return (
-    //         <div className="k-bubble">
-    //             <div>This is the message: {props.item.text}</div>
-    //         </div>
-    //     );
-    // };
-
-    const AttachmentTemplate = (props) => {
+    const CustomAttachmentTemplate = (props) => {
         let attachment = props.item;
         return (
             <a
@@ -291,6 +252,15 @@ const Conversation = () => {
         // }
     };
 
+    // const CustomMessageTemplate = (props) => {
+    //     return (
+    //         <div className="k-bubble">
+    //             <div>This is the message: {props.item.text}</div>
+    //         </div>
+    //     );
+    // };
+
+
     if (isLoading) {
         return <span>Loading...</span>
     }
@@ -298,8 +268,7 @@ const Conversation = () => {
     if (isError) {
         return <span>Error: {error.message}</span>
     }
-
-    console.log("Render conversation")
+    
     return (
         
             <Chat
@@ -307,10 +276,10 @@ const Conversation = () => {
                 messages={data}
                 onMessageSend={addNewMessage}
                 placeholder={"Type a message..."}
-                //messageTemplate={MessageTemplate}
+                //messageTemplate={CustomMessageTemplate}
                 messageBox={CustomMessage}
                 message={CustomChatMessage}
-                attachmentTemplate={AttachmentTemplate}
+                attachmentTemplate={CustomAttachmentTemplate}
                 width={"100%"}
             />
         
