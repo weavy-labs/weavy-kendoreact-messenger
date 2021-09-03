cd9dacdaf2e7fd603feb8ffd33b7d0c6c2d13b52
Johan Jeppson <johanjeppson@hotmail.com>
2021-09-03 16:50:36 +0200

    conversationslisting and css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
-import { Fragment, React } from "react";
+import { React } from 'react';
 import { NavLink } from "react-router-dom";
 import { ListView } from "@progress/kendo-react-listview";
 import { Avatar } from "@progress/kendo-react-layout";
-import { DropDownButton, DropDownButtonItem } from "@progress/kendo-react-buttons";
+import { DropDownButton, DropDownButtonItem, Button } from "@progress/kendo-react-buttons";
 import ConversationExcerpt from "./ConversationExcerpt";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { API_URL } from "../constants";
 
 const Sidebar = (props) => {
+  const queryClient = useQueryClient();
+
   const getConversations = async () => {
     const response = await fetch(API_URL + "/api/conversations", {
       method: "GET",
@@ -17,53 +19,85 @@ const Sidebar = (props) => {
     return conversations;
   };
 
-  const { isLoading, isError, data, error } = useQuery(
-    "conversations",
-    getConversations,
-    { refetchOnWindowFocus: false }
-  );
+  const { isLoading, isError, data, error } = useQuery("conversations", getConversations, { refetchOnWindowFocus: false });
+
+  const onPin = (id) => {
+    fetch(`${API_URL}/api/conversations/${id}/pin`, {
+      method: "POST",
+      credentials: "include",
+    }).then((response) => {
+      if (response.ok) {
+        // TODO: better way of doing this?
+        queryClient.invalidateQueries(["conversations"]);
+      } else {
+        console.error(`Failed to pin conversation: ${response.statusText} (${response.status})`);
+      }
+    });
+  };
+
+  const onUnPin = (id) => {
+    fetch(`${API_URL}/api/conversations/${id}/pin`, {
+      method: "DELETE",
+      credentials: "include",
+    }).then((response) => {
+      if (response.ok) {
+        // TODO: better way of doing this?
+        queryClient.invalidateQueries(["conversations"]);
+      } else {
+        console.error(`Failed to unpin conversation: HTTP ${response.status}`);
+      }
+    });
+  };
 
   const onClick = (event) => {
-    console.log(event.item.action);
+    console.log(event);
+    const actions = ["pin", "unpin", "star", "unstar", "read", "unread"];
+
+    if (actions.includes(event.item.action)) {
+      fetch(`${API_URL}/api/conversations/${event.item.id}/${event.item.action}`, {
+        method: "POST",
+        credentials: "include"
+      }).then((response) => {
+        if (response.ok) {
+          // TODO: better way of doing this?
+          queryClient.invalidateQueries(["conversations"]);
+        } else {
+          console.error(`Failed to ${event.item.action} conversation: ${response.statusText} (${response.status})`);
+        }
+      });
+    } else {
+        console.error("Unsupported action: " + event.item.action);
+    }
   };
 
   const MyItemRender = (props) => {
     let item = props.dataItem;
 
-    return (
-      <div className="conversation-list">
+    return (        
+      <div className= {!item.is_read ? "conversation-list unread" : "conversation-list"}>
         <NavLink to={"/conversation/" + item.id} activeClassName="active">
           <div className="row p-2 border-bottom align-middle" style={{ margin: 0 }}>
             <div className="col-2">
               <Avatar shape="circle" type="image">
-                <img
-                  alt=""
-                  src={
-                    API_URL + `/${item.avatar_url.replace("{options}", "48")}`
-                  }
-                />
+                <img alt="" src={`${API_URL}/${item.avatar_url.replace("{options}", "48")}`} />
               </Avatar>
             </div>
             <div className="col-10">
-              <time className="text-muted" dateTime="{item.created}" title="{item.created}">{item.created_as_string}</time>
+              <time className="text-muted" dateTime="{item.last_message_at}" title="{item.last_message_at}">
+                {item.last_message_at_string}
+              </time>
               <div className="text-truncate">{item.title}</div>
               <ConversationExcerpt id={item.id} excerpt={item.excerpt}></ConversationExcerpt>
             </div>
           </div>
         </NavLink>
-        <DropDownButton icon="cog" onItemClick={onClick}>
-            {item.is_starred
-            ? <DropDownButtonItem id={item.id} action="unstar" text="Unstar" icon="star-outline"></DropDownButtonItem>
-            : <DropDownButtonItem id={item.id} action="star" text="Star" icon="star"></DropDownButtonItem>
-            }
-            {item.is_pinned
-            ? <DropDownButtonItem id={item.id} action="unpin" text="Unpin" icon="unpin"></DropDownButtonItem>
-            : <DropDownButtonItem id={item.id} action="pin" text="Pin" icon="pin"></DropDownButtonItem>
-            }
-            {item.is_read 
-            ? <DropDownButtonItem id={item.id} action="read" text="Mark as unread" icon="checkbox"></DropDownButtonItem>
-            : <DropDownButtonItem id={item.id} action="unread" text="Mark as read" icon="checkbox-checked"></DropDownButtonItem>
-            }
+        {/* {item.is_starred &&
+            <Button icon="star" onClick={onClick} id={item.id} action="unstar"></Button>
+        } */}
+        <DropDownButton icon={item.is_pinned ? "pin" : "cog"} onItemClick={onClick}>
+          {item.is_starred ? <DropDownButtonItem id={item.id} action="unstar" text="Unstar" icon="star-outline"></DropDownButtonItem> : <DropDownButtonItem id={item.id} action="star" text="Star" icon="star"></DropDownButtonItem>}
+          {item.is_pinned ? <DropDownButtonItem id={item.id} action="unpin" text="Unpin" icon="unpin"></DropDownButtonItem> : <DropDownButtonItem id={item.id} action="pin" text="Pin" icon="pin"></DropDownButtonItem>}
+          {!item.is_read ? <DropDownButtonItem id={item.id} action="read" text="Mark as read" icon="checkbox"></DropDownButtonItem> : typeof item.last_message_at !== "undefined" ? <DropDownButtonItem id={item.id} action="unread" text="Mark as unread" icon="checkbox-checked"></DropDownButtonItem> : ""}            
         </DropDownButton>
       </div>
     );
