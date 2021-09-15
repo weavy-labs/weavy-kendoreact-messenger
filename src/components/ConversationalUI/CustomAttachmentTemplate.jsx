7ea99dde86599208e424f551bf1ecfaf001de5a2
Magnus Krona <magnus.krona@gmail.com>
2021-09-15 13:10:22 +0200

    more attachments

--- src/components/ConversationalUI/CustomAttachmentTemplate.jsx
+++ src/components/ConversationalUI/CustomAttachmentTemplate.jsx
@@ -1,56 +1,54 @@
 import { API_URL } from "../../constants";
 
-const CustomAttachmentTemplate = (props) => {
-        
+const DocumentAttachment = (props) => {
     return (
-        <a
-            href={API_URL + props.item.url}
-            target="_blank"
-            draggable={false}
-            tabIndex={-1}
-            rel="noopener noreferrer"
-        >
-            <img
-                alt=""
-                style={{
-                    width: 250,
-                }}
-                src={API_URL + "/attachments/" + props.item.id + "/image.png"}
-                draggable={false}
-            />
-        </a>
-    );
-    //  return (
-    //     <div className="k-card">
-    //         <div className="k-card-body">
-    //         <img alt="" src={"https://showcase.weavycloud.com/attachments/" + attachment + "/image.png"} draggable={false} rel="noopener noreferrer"/>;
-    //         </div>
-    //     </div>
-    // );
+        <div className="k-card">
+            <div className="k-card-body">
+                <a
+                    href={API_URL + props.item.download}
+                    target="_blank"
+                    draggable={false}
+                    tabIndex={-1}
+                    rel="noopener noreferrer"
+                >
+                     <img
+                        alt=""
+                        src={API_URL + `${props.item.thumb.replace("{options}", "32")}`}
+                        draggable={false}
+                    />
+                    {props.item.name}
+                </a>
+            </div>
+        </div>
 
-    // if (attachment.contentType === "link") {
-    //     return (
-    //         <div className="k-card">
-    //             <div className="k-card-body">
-    //                 <a
-    //                     href={attachment.site}
-    //                     target="_blank"
-    //                     draggable={false}
-    //                     tabIndex={-1}
-    //                     rel="noopener noreferrer"
-    //                 >
-    //                     {attachment.content}
-    //                 </a>
-    //             </div>
-    //         </div>
-    //     );
-    // } else if (attachment.contentType.match("^image")) {
-    //     return <img alt="" src={attachment.content} draggable={false} />;
-    // } else if (attachment.contentType === "text/plain") {
-    //     return attachment.content;
-    // } else {
-    //     return null;
-    // }
+    )
+}
+const ImageAttachment = (props) => {
+
+    return (
+        <div className="k-card">
+            <div className="k-card-body">
+                <a
+                    href={API_URL + props.item.download}
+                    target="_blank"
+                    draggable={false}
+                    tabIndex={-1}
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        alt=""
+                        src={API_URL + "/attachments/" + props.item.id + "/image.png"}
+                        draggable={false}
+                    />
+                </a>
+            </div>
+        </div>
+    );
+}
+
+const CustomAttachmentTemplate = (props) => {
+    return props.item.kind === "image" ? <ImageAttachment {...props} /> : <DocumentAttachment {...props} />;
+
 };
 
 export default CustomAttachmentTemplate;
